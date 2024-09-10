1d5b84a34336256d6c087f300bf44fc51f83d555
Polina Hodorchenko <polina.khadorchanka@gmail.com>
2024-09-10 17:49:40 +0300

    fixes

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -6,12 +6,15 @@ import './styles/index.css';
 import { store } from './store/store.ts';
 import { Provider } from 'react-redux';
 
+console.log(import.meta.env);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  //<React.StrictMode>
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  //</React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+    ,
+  </React.StrictMode>,
 );
